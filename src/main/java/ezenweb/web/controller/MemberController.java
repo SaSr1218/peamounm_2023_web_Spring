2691b4f4163dca4072b9f77f72c3fc6a23434a21
Sengah Choi <cntjr3@naver.com>
2023-04-10 12:26:37 +0900

    Day 69 Spring 6일차 / Spring web 회원가입 CRUD 기본 작성

--- src/main/java/ezenweb/web/controller/MemberController.java
+++ src/main/java/ezenweb/web/controller/MemberController.java
@@ -1,35 +1,52 @@
 package ezenweb.web.controller;
 
+import ezenweb.web.domain.member.MemberDto;
+import ezenweb.web.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @RequestMapping("/member")
 public class MemberController {
+    // 1. @Autowird 생략 할 경우 ( JSP에서 싱글톤 만드는 방식처럼..)
+    // MemberService service = new MemberService();
 
-    // 1. 회원가입
+    // 2. @Autowired 있을때 객체[빈] 자동 생성
+    @Autowired
+    MemberService memberService;
+
+    // 1. [C]회원가입
     @PostMapping("/info")
     public boolean write( @RequestBody MemberDto memberDto ){
-        return false;
+        log.info("member info write : " + memberDto );
+        boolean result = memberService.write(memberDto);
+        return result;
     }
 
-    // 2. 회원정보 호출
+    // 2. [R]회원정보 호출
     @GetMapping("/info")
     public MemberDto info( @RequestParam int mno ){
-        return null;
+        log.info("member info write : " + mno );
+        MemberDto result = memberService.info( mno );
+        return result;
     }
 
-    // 3. 회원정보 수정
+    // 3. [U]회원정보 수정
     @PutMapping("/info")
     public boolean update( @RequestBody MemberDto memberDto ) {
-        return false;
+        log.info("member info write : " + memberDto );
+        boolean result = memberService.update( memberDto );
+        return result;
     }
 
-    // 4. 회원정보 탈퇴
+    // 4. [D]회원정보 탈퇴
     @DeleteMapping("/info")
     public boolean delete( @RequestParam int mno ){
-        return false;
+        log.info("member info write : " + mno );
+        boolean result = memberService.delete( mno );
+        return result;
     }
 
 }
---
+++ src/main/java/ezenweb/web/domain/member/MemberEntityRepository.java
@@ -0,0 +1,6 @@
+package ezenweb.web.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberEntityRepository extends JpaRepository< MemberEntity , Integer> {
+}
