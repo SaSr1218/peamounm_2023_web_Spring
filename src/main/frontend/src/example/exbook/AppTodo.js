9e3d146be6e5a208da73fcc0f67a0076e7fdf0ca
Sengah Choi <cntjr3@naver.com>
2023-04-20 11:27:36 +0900

    Day 76 Spring 13일차 / @mui/material 통해 함수 작성 ( item 삭제 )

--- src/main/frontend/src/example/exbook/AppTodo.js
+++ src/main/frontend/src/example/exbook/AppTodo.js
@@ -7,7 +7,7 @@ import AddTodo from './AddTodo';
 export default function AppTodo( props ){
 
     // 1. // item 객체에 { id : "0" , title : "Hello World 1" , done : true } 초기값을 대입한 것
-    const [ items , setItems ] = useState (
+    const [ items , setItems ] = useState (           // 1번!_!
         [ // array start
 
             /* {
@@ -29,15 +29,22 @@ export default function AppTodo( props ){
         item.done = false;
         setItems([...items, item]); // 기존 상태 items 에 item 추가
         // setItems ( [ ...상태명, 추가할 데이터 ] );
-
     };
+
+    // 3. items에 저장된 item 삭제하는 함수
+    const deleteItem = (item) => {
+        // 만약에 items에 있는 아이템 중 id와 삭제할 id와 다른 경우 해당 item 반환
+        const newItems = items.filter( i=> i.id !== item.id);
+            // * 삭제할 id를 제외한 새로운 newItems 배열에 선언
+        setItems([...newItems]);
+    }
 
     // 반복문 이용한 Todo 컴포넌트 생성
     let TodoItems =
             <Paper style={{ margin : 16 }} >
                 <List>{
-                        items.map( (i) =>
-                            <Todo item = { i } key = { i.id } />
+                        items.map( (i) =>   // 2번 !_!
+                            <Todo item = { i } key = { i.id } 삭제함수 = {deleteItem} />
                         )
                     }
                 </List>
@@ -52,3 +59,15 @@ export default function AppTodo( props ){
         </div>
     </> );
 }
+
+        // JS 반복문 함수 제공
+            // r = [ 1, 2, 3 ]
+            // 배열/리스트.forEach( (o) => { } );    : 반복문만 가능 [ return X ]
+                // let array = r.forEach( ( o ) => { return o+3 } );
+                // 반복문이 끝나면 array에는 아무것도 들어있지 않다.
+            // 배열/리스트.map ( (o) => { } );       : return 값을 새로운 배열에 저장
+                // let array = r.map( ( o ) => { return o+3 } );
+                // 반복문이 끝나면 array에는 [ 4 , 5 , 6 ] 이 담겨져 있음.
+            // 배열/리스트.filter ( (o) => { } )     : 조건충족할경우 객체 반환
+                // let array = r.filter( ( o ) => { o >= 3 } );
+                // 반복문이 끝나면 array에는 [3] 이 담겨져 있음.
--- src/main/frontend/src/example/exbook/Todo.js
+++ src/main/frontend/src/example/exbook/Todo.js
@@ -1,12 +1,23 @@
 // p.183 컴포넌트 만들기
 import React , { useState } from 'react';
-import { ListItem , ListItemText , InputBase , Checkbox } from '@mui/material';
+import { ListItem , ListItemText , InputBase ,
+         Checkbox , ListItemSecondaryAction , IconButton } from '@mui/material';
+// 삭제 아이콘
+import DeleteOutlined from '@mui/icons-material/DeleteOutlined'
 
-export default function Todo(props){
+export default function Todo(props){ // 3번 !_!
     console.log( props )
 
-    // 1. HOOK 상태관리 userState
+    // 1. HOOK 상태관리 userState           // 4번 !_!
     const [ item, setItem ] = useState( props.item );
+
+    // 2. props 전달된 삭제함수 변수로 이동
+    const deleteItem = props.삭제함수;
+
+    // 3. 삭제함수 이벤트처리 핸들러
+    const deleteEventHandler = () => {
+        deleteItem(item); // 5번 !_!
+    }
 
     return( <>
         <ListItem>
@@ -21,6 +32,12 @@ export default function Todo(props){
                     fullWidth={true}
                 />
             </ListItemText>
+            <ListItemSecondaryAction>
+                <IconButton onClick={deleteEventHandler} >
+                    <DeleteOutlined />
+                </IconButton>
+            </ListItemSecondaryAction>
+
         </ListItem>
     </> );
 
