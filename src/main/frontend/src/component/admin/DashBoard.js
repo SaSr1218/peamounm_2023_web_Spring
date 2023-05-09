5e72d4e480563560c26e6a108206ad8887c44b26
Sengah Choi <cntjr3@naver.com>
2023-05-09 12:15:32 +0900

    Day 88 Spring 25일차 / 제품 CRUD

--- src/main/frontend/src/component/admin/DashBoard.js
+++ src/main/frontend/src/component/admin/DashBoard.js
@@ -1,25 +1,26 @@
 import React,{ useState , useEffect } from 'react';
 import axios from 'axios';
+import ProductTable from './ProductTable';
+import Container from '@mui/material/Container';
+
 export default function DashBoard( props ) {
-
-  const setCategory = () => {
-    console.log('setCategory 실행');
-    let cname = document.querySelector('.cname');
-    axios
-      .post('/board/category/write', { "cname": cname.value })
-      .then((r) => {
-        if (r.data === true) {
-          alert('카테고리 등록 성공');
-          cname.value = '';
-        }
-      });
-  };
-
+    // 1.카테고리 등록 버튼을 눌렀을때 이벤트
+    const setCategory = () => {    console.log('setCategory')
+        let cname = document.querySelector(".cname");
+        axios.post( '/board/category/write',{ "cname" : cname.value } )
+            .then( (r) => { console.log(r)
+                if( r.data == true ){ alert('카테고리 등록성공'); cname.value = '' }
+            })
+    }
     return(<>
-                <h3> 관리자 페이지 </h3>
-                <h6> 게시판 카테고리 추가 </h6>
-                <input type="text" className="cname" />
-                <button onClick={setCategory} type="button"> 카테고리 등록</button>
-    </>)
+        <Container >
+            <h3> 관리자 페이지 </h3>
+            <h6> 게시판 카테고리 추가 </h6>
+            <input type="text" className="cname" />
+            <button onClick={ setCategory } type="button"> 카테고리 등록</button>
 
+            <ProductTable />
+
+        </Container>
+    </>)
 }
